fa7b7ebb041510c0c37ad739462d8391bed9801b
Mehmet Sait DEMİR <demirmsait@gmail.com>
2020-10-08 18:53:17 +0300

    more change

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -2,4 +2,10 @@ public class AwesomeTest {
 
     //red more it is untraced
     // how is going this is hard part of my life
+
+    // ı just pulled the new change from github
+    // test
+    //public
+
+
 }
